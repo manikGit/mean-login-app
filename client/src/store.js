569506b5c08a2ddab6ba480e5ba9483d54a93ac9
Manik Srivastava <srivastavamanik2@gmail.com>
2018-12-30 13:23:08 +0530

    Update compose for production, if Redux extension tool is not installed

--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,25 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {};
 
 const middleware = [thunk];
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
-
+// let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+// window.__REDUX_DEVTOOLS_EXTENSION__();
+// if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+//     devTools = "";
+// }
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    devTools
+    // devTools
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+
   )
 );
 
